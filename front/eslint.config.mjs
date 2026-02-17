c5fa87a18e89f4763e8666effbd0f52a71635e36
nicoovillarr <60519222+nicoovillarr@users.noreply.github.com>
2026-02-17 12:47:20 -0300

    fix build

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-expressions': 'warn',
+    'prefer-const': 'warn',
+    'react/no-unescaped-entities': 'warn',
+    },
+  }),
 ];
 
 export default eslintConfig;
